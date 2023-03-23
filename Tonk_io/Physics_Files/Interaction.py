84d3e0ce96708bab8c2545c9eecfa4868a0c7240
TomAppleby <128051617+TomAppleby@users.noreply.github.com>
2023-03-23 11:55:58 +0000

    Update Interaction.py

--- Tonk_io/Physics_Files/Interaction.py
+++ Tonk_io/Physics_Files/Interaction.py
@@ -8,6 +8,8 @@ class Interaction:
                 if(enemy.hit(rocket)):
                     score = score + 1
                     rocketList.pop(rocketList.index(rocket))
+                    explosion(enemyList.index(enemy)) = Spritesheet(https://raw.githubusercontent.com/viiiscount/CS1822/main/Textures/dominic-garcia-tumblr-ojn6bb7cfk1w36evao1-1280.jpg, 3, 5, 16, enemy.pos.get_p(x), enemy.pos.get_p(y), 20)
+                    explosions.append(explosion(enemyList.index(enemy)))
                     enemyList.pop(enemyList.index(enemy))
                     
         return rocketList, enemyList, score
